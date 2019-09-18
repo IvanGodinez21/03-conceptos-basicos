a9a2468bc1a49c3ab48ec63fffe5d62b20a4a8bd
IvanGodinez21 <ivano.godinez@gmail.com>
2019-09-17 19:37:49 -0500

    Se añadieron nuevos archivos

---
+++ main.js
@@ -0,0 +1,47 @@
+/*
+Universidad de Colima
+Facultad de Telemática
+Fundamentos de programacion
+*/
+
+// Ejemplo de concepto básico
+
+var nombre = "Iván Godínez"
+var edad = 18
+var estudiante = true
+var email = "ivano.godinez@gmail.com"
+edadNumero = "dieciocho"
+
+console.log(
+    nombre + " tiene " + edad + " años. ¿Es estudiante? " + estudiante
+)
+console.log(nombre)
+console.log(edad)
+console.log(edadNumero)
+console.log(estudiante)
+console.log(email)
+
+alert("Hola " + nombre + " ¿Tienes " + edad + " años?, de lo contrario no podras acceder al sitio web")
+
+nombre = prompt ("Ingresa tu edad")
+nombre = prompt ("¿Cual es tu username?")
+
+var y = 1
+var x = y + 1
+
+console.log("x equivale a " + x)
+console.log("y equivale a "+ y)
+
+console.log(x + y + " Suma de x + y")
+console.log(x - y + " Resta de x - y")
+console.log(x * y + " Multiplicación de x * y")
+console.log(x / y + " División de x / y")
+
+var resultado = x + y
+console.log(resultado)
+
+console.log(50 + 10 / 8 + 2)
+console.log((50 + 10) / (8+2))
+
+
+
